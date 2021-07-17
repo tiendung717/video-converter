b635e5f8e04da96ca8dcce66dac028e9aeadce4c
nt.dung <dzung@edge.com>
2021-07-18 00:12:29 +0700

    Add sample stickers

--- feature_editor/src/main/java/com/goodmood/feature/editor/EditorConfig.kt
+++ feature_editor/src/main/java/com/goodmood/feature/editor/EditorConfig.kt
@@ -2,5 +2,5 @@ package com.goodmood.feature.editor
 
 object EditorConfig {
     const val DEFAULT_FONT_COLOR = "#FF0000"
-    const val DEFAULT_FONT_NAME = "louis.ttf"
+    const val DEFAULT_FONT_NAME = "sample.ttf"
 }
--- feature_editor/src/main/java/com/goodmood/feature/editor/repository/ResourceManager.kt
+++ feature_editor/src/main/java/com/goodmood/feature/editor/repository/ResourceManager.kt
@@ -7,7 +7,12 @@ import com.goodmood.platform.utils.FileUtils
 class ResourceManager(private val context: Context) {
 
     // Default font is "Louis.ttf" in assets
-    val fontFile by lazy { FileUtils.getFileFromAssets(context, EditorConfig.DEFAULT_FONT_NAME).absolutePath }
+    val fontFile by lazy {
+        FileUtils.getFileFromAssets(
+            context,
+            EditorConfig.DEFAULT_FONT_NAME
+        ).absolutePath
+    }
 
     // Default font color is RED
     val fontColor by lazy { EditorConfig.DEFAULT_FONT_COLOR }
@@ -15,7 +20,11 @@ class ResourceManager(private val context: Context) {
     // All stickers is in assets
     val stickerFiles by lazy {
         listOf<String>(
-            FileUtils.getFileFromAssets(context, "sticker.png").absolutePath
+            FileUtils.getFileFromAssets(context, "cool.png").absolutePath,
+            FileUtils.getFileFromAssets(context, "fun.png").absolutePath,
+            FileUtils.getFileFromAssets(context, "happy.png").absolutePath,
+            FileUtils.getFileFromAssets(context, "laughing.png").absolutePath,
+            FileUtils.getFileFromAssets(context, "dissapointment.png").absolutePath
         )
     }
 }
