ddd6582d2d5d61ae3fc8bf8a23a91dfff602fd6a
nt.dung <dzung@edge.com>
2021-07-18 00:42:09 +0700

    handle adding multiple stickers to video

--- feature_editor/src/main/java/com/goodmood/feature/editor/repository/filter/StickerFilter.kt
+++ feature_editor/src/main/java/com/goodmood/feature/editor/repository/filter/StickerFilter.kt
@@ -2,6 +2,8 @@ package com.goodmood.feature.editor.repository.filter
 
 import com.goodmood.core.ffmpeg.filter.FFmpegMergeFilter
 import com.goodmood.feature.editor.repository.model.Sticker
+import com.goodmood.platform.log.AppLog
+import java.lang.StringBuilder
 
 class StickerFilter(private val stickerList: List<Sticker>) : FFmpegMergeFilter() {
 
@@ -11,12 +13,40 @@ class StickerFilter(private val stickerList: List<Sticker>) : FFmpegMergeFilter(
             params.addAll(
                 listOf(
                     "-i",
-                    it.path,
-                    "-filter_complex",
-                    "overlay=${it.xPercent}*W:${it.yPercent}*H"
+                    it.path
                 )
             )
         }
+
+        params.add("-filter_complex")
+        if (stickerList.size == 1) {
+            val sticker = stickerList[0]
+            params.add("overlay=${sticker.xPercent}*W:${sticker.yPercent}*H")
+        } else {
+            val filterExpr = StringBuilder()
+            stickerList.forEachIndexed { index, sticker ->
+                when (index) {
+                    0 -> {
+                        filterExpr.append("[0][${index + 1}]")
+                        filterExpr.append("overlay=${sticker.xPercent}*W:${sticker.yPercent}*H")
+                        filterExpr.append("[label${index}]")
+                        filterExpr.append(";")
+                    }
+                    stickerList.size - 1 -> {
+                        filterExpr.append("[label${index - 1}][${index + 1}]")
+                        filterExpr.append("overlay=${sticker.xPercent}*W:${sticker.yPercent}*H")
+                    }
+                    else -> {
+                        filterExpr.append("[label${index - 1}][${index + 1}]")
+                        filterExpr.append("overlay=${sticker.xPercent}*W:${sticker.yPercent}*H")
+                        filterExpr.append("[label${index}]")
+                        filterExpr.append(";")
+                    }
+                }
+            }
+            params.add(filterExpr.toString())
+        }
+
         return params
     }
 }
